adaa41e590ff98bd889a5d9448f547c143732fbe
stepan <stp.eternal@gmail.com>
2013-07-16 14:27:31 +0600

    a bit of refactoring in 1643

--- lib/1643.rb
+++ lib/1643.rb
@@ -47,18 +47,12 @@ class Land
   end
 
   def region(row, col)
-    if row >= 0 && row < @rows && col >= 0 && col < @cols
-      @configuration[row][col]
-    else
-      IMPASSABLE
-    end
+    @configuration[row][col]
   end
 
   def passable?(row, col)
     region(row, col) != IMPASSABLE
   end
-
-  SURROUNDINGS = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
 
   def search_paths_to_sandro_surroundings(from)
     map = Array.new(@rows){ Array.new @cols }
@@ -66,10 +60,8 @@ class Land
     visited = [from]
     while passable_sandro_surroundings.any?{|s| map[s[0]][s[1]].nil? } && !visited.empty?
       current = visited.shift
-      SURROUNDINGS.each do |d_row, d_col|
-        row = current[0] + d_row
-        col = current[1] + d_col
-        if map[row] && map[row][col].nil? && passable?(row, col) && !(row == @sandro[0] && col == @sandro[1])
+      surroundings(*current) do |row, col|
+        if map[row][col].nil? && passable?(row, col) && !(row == @sandro[0] && col == @sandro[1])
           d = map[current[0]][current[1]] + 1
           map[row][col] = d
           visited << [row, col]
@@ -96,13 +88,19 @@ class Land
   def passable_sandro_surroundings
     @passable_sandro_surroundings ||= begin
       s = []
-      SURROUNDINGS.each do |d_row, d_col|
-        row = @sandro[0] + d_row
-        col = @sandro[1] + d_col
-        s << [row,col] if passable?(row, col)
-      end
+      surroundings(*@sandro){|*reg| s << reg if passable?(*reg) }
       s
     end
+  end
+
+  SURROUNDINGS = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+
+  def surroundings(row, col)
+    SURROUNDINGS.each do |d_row, d_col|
+      r = row + d_row
+      c = col + d_col
+      yield r, c if r >= 0 && r < @rows && c >= 0 && c < @cols
+    end    
   end
 
 end
