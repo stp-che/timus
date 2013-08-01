4742b1f9012bfdee3425bdb8d8b39b977b2b755f
Stepan Chebannyj <stp.eternal@gmail.com>
2013-08-01 07:52:46 +0600

    fix 1196

--- lib/1196.rb
+++ lib/1196.rb
@@ -1,21 +1,20 @@
-data = $stdin.read.split("\n").map{|l| l.to_i}
+DATA = $stdin.read.split("\n").map(&:to_i)
 
-N = data[0]
-CHECK_LIST = data[1..N]
+N = DATA[0]
 
 $count = 0
 
-def check(value, from=0, to=N-1)
-  return false if value > CHECK_LIST[to] || value < CHECK_LIST[from]
-  return true if value == CHECK_LIST[from]
+def check(value, from=1, to=N)
+  return false if value > DATA[to] || value < DATA[from]
+  return true if value == DATA[from]
   m = (from+to)/2
-  value > CHECK_LIST[m] ? check(value, m+1, to) : check(value, from, m)
+  value > DATA[m] ? check(value, m+1, to) : check(value, from, m)
 end
 
-M = data[N+1]
+M = DATA[N+1]
 i = N+2
 M.times{ 
-  $count += 1 if check(data[i])
+  $count += 1 if check(DATA[i])
   i += 1
 }
 
