fee989c9f5378485398a0936be052bd751b9071f
Stepan Chebannyj <stp.eternal@gmail.com>
2013-08-14 07:32:05 +0600

    problem 1194

---
+++ examples/1194.rb
@@ -0,0 +1,21 @@
+# http://acm.timus.ru/problem.aspx?space=1&num=1194
+
+Problem.describe '1194' do |pr|
+
+  pr.example ['3 0',
+              '1 2 2 2 3 1',
+              '2 2 4 1 5 1']*"\n", 
+
+              '3'
+
+  pr.example ['2 1'], '0'
+
+  pr.example ['6 1',
+              '1 3 2 2 3 1 4 3',
+              '2 2 5 1 6 1',
+              '4 2 7 1 8 2']*"\n",
+
+              '14'
+
+
+end
