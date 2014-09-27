5fc2cae0f017af62bfd1b261403bdf2a4bd66fb8
Stepan Chebannyj <stp.eternal@gmail.com>
2014-09-27 14:49:12 +0600

    a bit of refactoring in 1881

--- lib/1881.scala
+++ lib/1881.scala
@@ -6,12 +6,15 @@ object Pr1881 {
         if (wordsLeft == 0) pagesCount
         else {
           val w_len = readLine().length
-          if (w_len <= lineSpaceLeft) 
-            iter(pagesCount, linesLeft, lineSpaceLeft-w_len-1, wordsLeft-1)
+
+          val p = if (w_len <= lineSpaceLeft) 
+            (pagesCount, linesLeft, lineSpaceLeft-w_len-1)
           else if (linesLeft > 0)
-            iter(pagesCount, linesLeft-1, pWidth-w_len-1, wordsLeft-1)
+            (pagesCount, linesLeft-1, pWidth-w_len-1)
           else
-            iter(pagesCount+1, pHeight-1, pWidth-w_len-1, wordsLeft-1)
+            (pagesCount+1, pHeight-1, pWidth-w_len-1)
+            
+          iter(p._1, p._2, p._3, wordsLeft-1)
         }
       }
       iter(1, pHeight-1, pWidth, wordsCount)
